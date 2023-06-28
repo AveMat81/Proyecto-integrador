c134ee0a7cbf53ee59a040ecfcd549fcee33c17c
AveMat81 <matiasavesani@yahoo.com.ar>
2023-06-27 21:46:14 -0300

    Primer commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from './Card';
+
+export default function Cards(props) {
+   return (
+    <div>
+      {props.characters.map((pj) =>(
+           <Card 
+            key={pj.id}
+            name={pj.name}
+            status={pj.status}
+            species={pj.species}
+            gender={pj.gender}
+            origin={pj.origin.name}
+            image={pj.image}
+            onClose={() => window.alert('Emulamos que se cierra la card')}
+           />
+   ) )
+     }
+    </div>
+   )   
+}
